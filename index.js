0160101c2c859ee25b71bceedbe83c06a17f72dc
AProductiveNerd <69358844+AProductiveNerd@users.noreply.github.com>
2021-08-24 11:34:58 +0530

    using async iife

--- index.js
+++ index.js
@@ -14,47 +14,52 @@
   let peak = 0;
 
   setInterval(() => {
-    const number = parseInt(
-      document
-        .getElementsByClassName("roster-list-title")[1]
-        ?.ariaLabel?.split("Attendees ")[1]
-    );
+    (async () => {
+      const number = await parseInt(
+        await document
+          .getElementsByClassName("roster-list-title")[1]
+          ?.ariaLabel?.split("Attendees ")[1]
+      );
 
-    let threshold = 10;
+      let threshold = 10;
 
-    if (number) {
-      peak < number ? (peak = number) : (peak = peak);
+      if (number) {
+        peak < number ? (peak = number) : (peak = peak);
 
-      if (peak > 50) {
-        threshold = 30;
-      } else if (peak > 32) {
-        threshold = 22;
-      } else if (peak > 22) {
-        threshold = 18;
-      } else {
-        threshold = 12;
-      }
-
-      console.log({ peak, threshold, number });
-
-      if (number <= threshold) {
-        document.getElementById("hangup-button").click();
-        console.log("hung up");
-        peak = 0;
-        threshold = 10;
-      }
-    } else {
-      const roster_panel = document.getElementsByTagName("calling-roster")[0];
-
-      if (!roster_panel || roster_panel.className.includes("ng-hide")) {
-        const roster_button = document.getElementById("roster-button");
-
-        if (!roster_button.ariaLabel.includes("Hide participants")) {
-          roster_button.click();
+        if (peak > 50) {
+          threshold = 30;
+        } else if (peak > 32) {
+          threshold = 22;
+        } else if (peak > 22) {
+          threshold = 18;
+        } else {
+          threshold = 12;
         }
 
-        console.log("opened roster panel");
+        console.log({ peak, threshold, number });
+
+        if (number <= threshold) {
+          await document.getElementById("hangup-button")?.click();
+
+          console.log("hung up");
+
+          peak = 0;
+          threshold = 10;
+        }
+      } else {
+        const roster_panel = await document.getElementsByTagName(
+          "calling-roster"
+        )[0];
+
+        if (!roster_panel && !roster_panel?.className?.includes("ng-hide")) {
+          const roster_button = await document.getElementById("roster-button");
+
+          if (!roster_button?.ariaLabel?.includes("Hide participants")) {
+            await roster_button?.click();
+          }
+          console.log("opened roster panel");
+        }
       }
-    }
+    })();
   }, 5000);
 })();
